e3eb31713ed812dc717a8cfe2729fbf9efb013db
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 19:24:35 +0000

    Update config

--- main.tf
+++ main.tf
@@ -15,6 +15,14 @@ provider "aws" {
 resource "aws_instance" "EC2" {
   ami           = "ami-0767046d1677be5a0"
   instance_type = "t2.micro"
+  subnet_id = aws_subnet.x3iibits-subnet-public-1.id
+  vpc_security_group_ids = [aws_security_group.x3iibits-all.id]
+  key_name = "EC2 instance"
+  
+  tags = {
+    "Name" = "linux-instance"
+  }
+  
 }
 
 
