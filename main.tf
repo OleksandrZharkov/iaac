8f61f9ef3ac1cddab0986de18cd31a4bfdb47740
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 18:40:38 +0000

    first commit

---
+++ main.tf
@@ -0,0 +1,54 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.22.0"
+    }
+  }
+  required_version = "~> 0.14"
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_uuid" "randomid" {}
+
+resource "aws_iam_user" "circleci" {
+  name = var.user
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "circleci" {
+  user = aws_iam_user.circleci.name
+}
+
+data "template_file" "circleci_policy" {
+  template = file("circleci_s3_access.tpl.json")
+  vars = {
+    s3_bucket_arn = aws_s3_bucket.app.arn
+  }
+}
+
+resource "local_file" "circle_credentials" {
+  filename = "tmp/circleci_credentials"
+  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
+}
+
+resource "aws_iam_user_policy" "circleci" {
+  name   = "AllowCircleCI"
+  user   = aws_iam_user.circleci.name
+  policy = data.template_file.circleci_policy.rendered
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+}
+
+resource "aws_instance" "EC2" {
+  ami           = "ami-0767046d1677be5a0"
+  instance_type = "t2.micro"
+}
+
+
