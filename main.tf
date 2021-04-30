d02cdecd4ec52b8f431fd3e0f40b0142d1158704
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 18:57:54 +0000

    Update config

--- main.tf
+++ main.tf
@@ -11,29 +11,7 @@ terraform {
 provider "aws" {
   region = var.region
 }
-
-resource "random_uuid" "randomid" {}
-
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
-
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
-
-
-resource "local_file" "circle_credentials" {
-  filename = "tmp/circleci_credentials"
-  content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
-}
-
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-}
-
+ 
 resource "aws_instance" "EC2" {
   ami           = "ami-0767046d1677be5a0"
   instance_type = "t2.micro"
