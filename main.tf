938bf7ac91cd704704d6d101eff57f58050fbf15
OleksandrZharkov <ozharkov.88@gmail.com>
2021-04-30 19:38:46 +0000

    Update  main

--- main.tf
+++ main.tf
@@ -17,7 +17,7 @@ resource "aws_instance" "linux-instance" {
   ami           = "ami-009b16df9fcaac611"
   instance_type = "t2.micro"
 
-  subnet_id = aws_subnet.x3iibits-subnet-public-3.id
+  subnet_id = aws_subnet.x3iibits-subnet-public-1.id
 
   vpc_security_group_ids = [aws_security_group.x3iibits-all.id]
 
@@ -26,5 +26,3 @@ resource "aws_instance" "linux-instance" {
   tags = {
     "Name" = "linux-instance"
   }
-
-
